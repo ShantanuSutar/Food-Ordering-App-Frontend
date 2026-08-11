d0d0ca789e9474e6f7eed0a462d348864382ab78
Shantanu Sutar <shantanusutar17@gmail.com>
2026-08-11 17:35:17 +0530

    feat: added Profile Navigation section

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import { CssBaseline } from '@mui/material'
 import { Home } from './components/Home/Home'
 import RestaurantDetails from './components/Restaurant/RestaurantDetails'
 import Cart from './components/Cart/Cart'
+import Profile from './components/Profile/Profile'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -21,7 +22,8 @@ function App() {
         <Navbar />
         {/* <Home/> */}
         {/* <RestaurantDetails/> */}
-        <Cart/>
+        {/* <Cart/> */}
+        <Profile/>
       </ThemeProvider>
     </>
   )
---
+++ src/components/Profile/ProfileNavigation.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag'
+import FavouriteIcon from '@mui/icons-material/FavoriteOutlined'
+import HomeIcon from '@mui/icons-material/Home'
+import AccountBalanceIcon from '@mui/icons-material/AccountBalanceWallet'
+import NotificationsIcon from '@mui/icons-material/NotificationsActive'
+import EventIcon from '@mui/icons-material/Event'
+import LogoutIcon from '@mui/icons-material/Logout'
+import { Divider, Drawer, useMediaQuery } from '@mui/material'
+
+const menu = [
+    { title: "Orders", icon: <ShoppingBagIcon /> },
+    { title: "Favourites", icon: <FavouriteIcon /> },
+    { title: "Address", icon: <HomeIcon /> },
+    { title: "Payments", icon: <AccountBalanceIcon /> },
+    { title: "Notifications", icon: <NotificationsIcon /> },
+    { title: "Events", icon: <EventIcon /> },
+    { title: "Logout", icon: <LogoutIcon /> },
+
+]
+
+const ProfileNavigation = ({ open, handleClose }) => {
+    const isSmallScreen = useMediaQuery("(max-width: 900px)");
+
+    return (
+        <div>
+            <Drawer anchor='left' open={isSmallScreen ? open : true} onClose={handleClose} variant={isSmallScreen ? "temporary" : "permanent"} sx={{ zIndex: 1 }}>
+                <div className=' w-[50vw] lg:w-[20vw] h-[100vh] flex flex-col justify-center text-xl gap-8 pt-16'>
+                    {
+                        menu.map((item, i) => <>
+                            <div className=' px-5 flex items-center space-x-5 cursor-pointer'>
+                                {item.icon}
+                                <span>{item.title}</span>
+                            </div>
+                            {i !== menu.length - 1 && <Divider />}
+                        </>)
+                    }
+                </div>
+            </Drawer>
+        </div>
+    )
+}
+
+export default ProfileNavigation
