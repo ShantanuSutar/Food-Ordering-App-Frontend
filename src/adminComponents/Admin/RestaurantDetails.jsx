a7fc2039df2bf3f342c9aa46fd52249dd2e50e86
Shantanu Sutar <shantanusutar17@gmail.com>
2026-08-19 13:01:09 +0530

    feat: added menu food category and ingredients table

--- src/adminComponents/Admin/RestaurantDetails.jsx
+++ src/adminComponents/Admin/RestaurantDetails.jsx
@@ -1,7 +1,20 @@
+import { Button } from '@mui/material'
 import React from 'react'
 
 export const RestaurantDetails = () => {
+  const handleRestaurantStatus = () => {
+    
+  }
   return (
-    <div>RestaurantDetails</div>
+    <div className=' lg:px-20 px-5'>
+      <div className=' py-5 flex justify-center items-center gap-5'>
+        <h1 className=' text-2xl lg:w-7xl text-center font-black p-5'>Indian Fast Food</h1>
+        <div>
+          <Button color={true ? "primary" : "error"} className=' py-[1rem] px-[2rem]' variant='contained' onClick={handleRestaurantStatus} size='large'>
+            {true ? "close" : "open"}
+          </Button>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/adminComponents/Ingredients/Ingredients.jsx
+++ src/adminComponents/Ingredients/Ingredients.jsx
@@ -1,7 +1,19 @@
+import { Grid } from '@mui/material'
 import React from 'react'
+import { IngredientsTable } from './IngredientsTable'
+import { IngredientsCategoryTable } from './IngredientsCategoryTable'
 
 export const Ingredients = () => {
   return (
-    <div>Ingredients</div>
+    <div className=' px-2'>
+      <Grid container spacing={2}>
+        <Grid size={{ xs: 12, lg: 8 }}>
+          <IngredientsTable />
+        </Grid>
+        <Grid size={{ xs: 12, lg: 4 }}>
+          <IngredientsCategoryTable />
+        </Grid>
+      </Grid>
+    </div>
   )
 }
