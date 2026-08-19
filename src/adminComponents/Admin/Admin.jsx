ca5a7595ae7047014cd39e83f5fe3bf3519ea087
Shantanu Sutar <shantanusutar17@gmail.com>
2026-08-19 12:09:02 +0530

    feat: added admin Order table

--- src/adminComponents/Admin/Admin.jsx
+++ src/adminComponents/Admin/Admin.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import { AdminSidebar } from './AdminSidebar'
+import { Route, Routes } from 'react-router-dom'
+import { RestaurantDashboard } from '../Dashboard/Dashboard'
+import { Orders } from '../Orders/Orders'
+import { Menu } from '../Menu/Menu'
+import { FoodCategory } from '../FoodCategory/FoodCategory'
+import { Ingredients } from '../Ingredients/Ingredients'
+import { Events } from '../Events/Events'
+import { RestaurantDetails } from './RestaurantDetails'
 
 export const Admin = () => {
   const handleClose = () => {
@@ -13,7 +21,16 @@ export const Admin = () => {
           <AdminSidebar handleClose={handleClose} />
         </div>
         <div className=' lg:w-[80%]'>
-
+          <Routes>
+            <Route path='/' element={<RestaurantDashboard/>} />
+            <Route path='/orders' element={<Orders/>} />
+            <Route path='/menu' element={<Menu/>} />
+            <Route path='/category' element={<FoodCategory/>} />
+            <Route path='/menu' element={<Menu/>} />
+            <Route path='/ingredients' element={<Ingredients/>} />
+            <Route path='/event' element={<Events/>} />
+            <Route path='/details' element={<RestaurantDetails/>} />
+          </Routes>
         </div>
       </div>
     </div>
