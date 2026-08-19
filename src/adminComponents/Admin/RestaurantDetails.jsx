9f4451236d6f5fa4aab8f640dec46c0c380c6b1b
Shantanu Sutar <shantanusutar17@gmail.com>
2026-08-19 15:32:04 +0530

    feat: added restaurant details

--- src/adminComponents/Admin/RestaurantDetails.jsx
+++ src/adminComponents/Admin/RestaurantDetails.jsx
@@ -1,19 +1,162 @@
-import { Button } from '@mui/material'
+import { Button, Card, CardContent, CardHeader, Grid } from '@mui/material'
 import React from 'react'
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+
 
 export const RestaurantDetails = () => {
   const handleRestaurantStatus = () => {
-    
+
   }
   return (
-    <div className=' lg:px-20 px-5'>
-      <div className=' py-5 flex justify-center items-center gap-5'>
-        <h1 className=' text-2xl lg:w-7xl text-center font-black p-5'>Indian Fast Food</h1>
-        <div>
-          <Button color={true ? "primary" : "error"} className=' py-[1rem] px-[2rem]' variant='contained' onClick={handleRestaurantStatus} size='large'>
-            {true ? "close" : "open"}
+    <div className=' lg:px-20 px-5 pb-10'>
+      <div className=' py-5 flex flex-col justify-center items-center gap-5'>
+        <div className='py-5 flex justify-center items-center gap-6'>
+          <h1 className='text-2xl font-black'>
+            Indian Fast Food
+          </h1>
+
+          <Button
+            color={true ? "primary" : "error"}
+            variant='contained'
+            onClick={handleRestaurantStatus}
+            size='large'
+          >
+            {true ? "Close" : "Open"}
           </Button>
         </div>
+        <Grid container spacing={2}>
+          <Grid item size={{ xs: 12 }}>
+            <Card>
+              <CardHeader title={<span className=' text-gray-300'>Restaurant</span>} />
+            </Card>
+            <CardContent>
+              <div className=' space-y-4 text-gray-200'>
+                <div className=' flex '>
+                  <p className=' w-48'>Owner</p>
+                  <p className=' text-gray-400'>
+                    <span className=' pr-5'>-</span>
+                    Shantanu
+                  </p>
+                </div>
+                <div className=' flex '>
+                  <p className=' w-48'>Restaurant Name</p>
+                  <p className=' text-gray-400'>
+                    <span className=' pr-5'>-</span>
+                    indian fast food
+                  </p>
+                </div>
+                <div className=' flex '>
+                  <p className=' w-48'>Cuisine Type</p>
+                  <p className=' text-gray-400'>
+                    <span className=' pr-5'>-</span>
+                    indian
+                  </p>
+                </div>
+                <div className=' flex '>
+                  <p className=' w-48'>Opening Hours</p>
+                  <p className=' text-gray-400'>
+                    <span className=' pr-5'>-</span>
+                    monday - sunday 9am - 9pm
+                  </p>
+                </div>
+                <div className=' flex '>
+                  <p className=' w-48'>Status</p>
+                  <p className=' text-gray-400'>
+                    <span className=' pr-5'>-</span>
+                    {true ? <span className=' px-5 py-2 rounded-full bg-green-400 text-gray-950'>Open</span>
+                      : <span className=' px-5 py-2 rounded-full bg-red-400 text-gray-950'>Closed</span>}
+                  </p>
+                </div>
+              </div>
+            </CardContent>
+          </Grid>
+          <Grid item size={{ xs: 12, lg: 6 }}>
+            <Card>
+              <CardHeader title={<span className=' text-gray-300'>Address</span>} />
+            </Card>
+            <CardContent>
+              <div className=' space-y-4 text-gray-200'>
+                <div className=' flex '>
+                  <p className=' w-48'>Country</p>
+                  <p className=' text-gray-400'>
+                    <span className=' pr-5'>-</span>
+                    Shantanu
+                  </p>
+                </div>
+                <div className=' flex '>
+                  <p className=' w-48'>City</p>
+                  <p className=' text-gray-400'>
+                    <span className=' pr-5'>-</span>
+                    indian fast food
+                  </p>
+                </div>
+                <div className=' flex '>
+                  <p className=' w-48'>Postal Code</p>
+                  <p className=' text-gray-400'>
+                    <span className=' pr-5'>-</span>
+                    indian
+                  </p>
+                </div>
+                <div className=' flex '>
+                  <p className=' w-48'>Street Address</p>
+                  <p className=' text-gray-400'>
+                    <span className=' pr-5'>-</span>
+                    monday - sunday 9am - 9pm
+                  </p>
+                </div>
+                
+              </div>
+            </CardContent>
+          </Grid>
+          <Grid item size={{ xs: 12, lg: 6 }}>
+            <Card>
+              <CardHeader title={<span className=' text-gray-300'>Contact</span>} />
+            </Card>
+            <CardContent>
+              <div className=' space-y-4 text-gray-200'>
+                <div className=' flex '>
+                  <p className=' w-48'>Email</p>
+                  <p className=' text-gray-400'>
+                    <span className=' pr-5'>-</span>
+                    Shantanu
+                  </p>
+                </div>
+                <div className=' flex '>
+                  <p className=' w-48'>Mobile</p>
+                  <p className=' text-gray-400'>
+                    <span className=' pr-5'>-</span>
+                    indian fast food
+                  </p>
+                </div>
+                <div className=' flex '>
+                  <p className=' w-48'>Social</p>
+                  <p className=' text-gray-400 flex items-center pb-3 gap-2'>
+                    <span className=' pr-5'>-</span>
+                    <div>
+                      <span className=' pr-5'>-</span>
+                      <a href="/">
+                        <InstagramIcon sx={{fontSize: "3rem"}} />
+                      </a>
+                      <a href="/">
+                        <TwitterIcon sx={{fontSize: "3rem"}} />
+                      </a>
+                      <a href="/">
+                        <LinkedInIcon sx={{fontSize: "3rem"}} />
+                      </a>
+                      <a href="/">
+                        <FacebookIcon sx={{fontSize: "3rem"}} />
+                      </a>
+                    </div>
+                  </p>
+                </div>
+                
+              </div>
+            </CardContent>
+          </Grid>
+        </Grid>
       </div>
     </div>
   )
--- src/adminComponents/CreateRestaurantForm/CreateRestaurantForm.jsx
+++ src/adminComponents/CreateRestaurantForm/CreateRestaurantForm.jsx
@@ -1,7 +1,164 @@
-import React from 'react'
+import { Button, CircularProgress, Grid, IconButton, TextField } from '@mui/material';
+import { AddPhotoAlternate, Close } from '@mui/icons-material';
+
+import { useFormik } from 'formik'
+import React, { useState } from 'react'
+
+const initialValues = {
+  name: "",
+  description: "",
+  cuisineType: "",
+  streetAddress: "",
+  city: "",
+  stateProvince: "",
+  postalCode: "",
+  country: "",
+  email: "",
+  mobile: "",
+  twitter: "",
+  instagram: "",
+  openingHours: "Mon-Sun : 9:00 AM - 12:00 PM",
+  images: []
+}
 
 export const CreateRestaurantForm = () => {
+  const [uploadImage, setUploadImage] = useState(false)
+  const formik = useFormik({
+    initialValues,
+    onSubmit: (values) => {
+      const data = {
+        name: values.name,
+        description: values.description,
+        cuisineType: values.cuisineType,
+        address: {
+          city: values.city,
+          stateProvince: values.stateProvince,
+          postalCode: values.postalCode,
+          country: values.country
+        },
+        contactInformation: {
+          email: values.email,
+          mobile: values.mobile,
+          twitter: values.twitter,
+          instagram: values.instagram
+        },
+        openingHours: values.openingHours,
+        images: values.images
+      }
+      console.log("data " , data)
+    }
+  });
+
+  const handleImageChange = (e) => {
+
+  }
+
+  const handleRemoveImage = (index) => {
+
+  }
+
   return (
-    <div>CreateRestaurantForm</div>
+    <div className=' py-10 px-5 lg:flex items-center justify-center min-h-screen'>
+      <div className=' lg:max-w-4xl'>
+        <h1 className=' font-bold text-2xl text-center py-2'>
+          Add new restaurant
+        </h1>
+        <form onSubmit={formik.handleSubmit} className=' space-y-4'>
+          <Grid container spacing={2}>
+            <Grid item size={{ xs: 12 }}>
+              <div className="flex flex-wrap gap-4">
+
+                {/* Upload button */}
+                <div className="relative">
+                  <input
+                    type="file"
+                    accept="image/*"
+                    id="fileInput"
+                    style={{ display: "none" }}
+                    onChange={handleImageChange}
+                  />
+
+                  <label htmlFor="fileInput">
+                    <span className="w-24 h-24 cursor-pointer flex items-center justify-center p-3 border rounded-md border-gray-600">
+                      <AddPhotoAlternate className="text-white" />
+                    </span>
+
+                    {uploadImage && (
+                      <div className="absolute inset-0 w-24 h-24 flex items-center justify-center">
+                        <CircularProgress />
+                      </div>
+                    )}
+                  </label>
+                </div>
+
+                {/* Images */}
+                {[1, 1, 1, 1].map((image, index) => (
+                  <div key={index} className="relative w-24 h-24">
+                    <img
+                      className="w-24 h-24 object-cover rounded-md"
+                      src="https://images.pexels.com/photos/1639562/pexels-photo-1639562.jpeg"
+                      alt=""
+                    />
+
+                    <IconButton
+                      size="small"
+                      sx={{
+                        position: "absolute",
+                        top: 0,
+                        right: 0,
+                        outline: "none",
+                      }}
+                      onClick={() => handleRemoveImage(index)}
+                    >
+                      <Close sx={{ fontSize: "1rem" }} />
+                    </IconButton>
+                  </div>
+                ))}
+              </div>
+            </Grid>
+            <Grid item size={{ xs: 12 }}>
+              <TextField fullWidth id='name' name='name' label="Name" variant='outlined' onChange={formik.handleChange} value={formik.values.name} />
+            </Grid>
+            <Grid item size={{ xs: 12 }}>
+              <TextField fullWidth id='description' name='description' label="Description" variant='outlined' onChange={formik.handleChange} value={formik.values.description} />
+            </Grid>
+            <Grid item size={{ xs: 12, lg: 6 }}>
+              <TextField fullWidth id='cuisineType' name='cuisineType' label="CuisineType" variant='outlined' onChange={formik.handleChange} value={formik.values.cuisineType} />
+            </Grid>
+            <Grid item size={{ xs: 12, lg: 6 }}>
+              <TextField fullWidth id='openingHours' name='openingHours' label="Opening Hours" variant='outlined' onChange={formik.handleChange} value={formik.values.openingHours} />
+            </Grid>
+            <Grid item size={{ xs: 12 }}>
+              <TextField fullWidth id='streetAddress' name='streetAddress' label="Street Address" variant='outlined' onChange={formik.handleChange} value={formik.values.streetAddress} />
+            </Grid>
+            <Grid item size={{ xs: 12 }}>
+              <TextField fullWidth id='city' name='city' label="City" variant='outlined' onChange={formik.handleChange} value={formik.values.city} />
+            </Grid>
+            <Grid item size={{ xs: 12, lg: 4 }}>
+              <TextField fullWidth id='stateProvince' name='stateProvince' label="State Province" variant='outlined' onChange={formik.handleChange} value={formik.values.stateProvince} />
+            </Grid>
+            <Grid item size={{ xs: 12, lg: 4 }}>
+              <TextField fullWidth id='postalCode' name='postalCode' label="Postal Code" variant='outlined' onChange={formik.handleChange} value={formik.values.postalCode} />
+            </Grid>
+            <Grid item size={{ xs: 12, lg: 4 }}>
+              <TextField fullWidth id='country' name='country' label="Country" variant='outlined' onChange={formik.handleChange} value={formik.values.country} />
+            </Grid>
+            <Grid item size={{ xs: 12, lg: 6 }}>
+              <TextField fullWidth id='email' name='email' label="Email" variant='outlined' onChange={formik.handleChange} value={formik.values.email} />
+            </Grid>
+            <Grid item size={{ xs: 12, lg: 6 }}>
+              <TextField fullWidth id='mobile' name='mobile' label="Mobile" variant='outlined' onChange={formik.handleChange} value={formik.values.mobile} />
+            </Grid>
+            <Grid item size={{ xs: 12, lg: 6 }}>
+              <TextField fullWidth id='instagram' name='instagram' label="Instagram" variant='outlined' onChange={formik.handleChange} value={formik.values.instagram} />
+            </Grid>
+            <Grid item size={{ xs: 12, lg: 6 }}>
+              <TextField fullWidth id='twitter' name='twitter' label="Twitter" variant='outlined' onChange={formik.handleChange} value={formik.values.twitter} />
+            </Grid>
+          </Grid>
+          <Button className='' variant='contained' color='primary' type='submit'>Create Restaurant</Button>
+        </form>
+      </div>
+    </div>
   )
 }
